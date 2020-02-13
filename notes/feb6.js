a10177156d7de264e8a22f10cf5bfbb482037f80
athinasaid <asaid@bu.edu>
2020-02-13 11:25:31 -0500

    feb6

--- notes/feb6.js
+++ notes/feb6.js
@@ -1,51 +1,51 @@
-//do not use var and do not use eval() EVER!!!
-
-const p1 = str => str.split('').sort().join('')
-console.log(`PS1: ${p1('supercalifragilisticexpialidocious')}`)
-
-const p2 = expression => {
-    const operator = expression.charAt(1);
-    switch (operator) {
-        case '+': return (expression) => Number(expression.charAt(0)) + Number(expression.charAt(2));
-        case '-': return (expression) => Number(expression.charAt(0)) - Number(expression.charAt(2));
-        case '*': return (expression) => Number(expression.charAt(0)) * Number(expression.charAt(2));
-        case '/': return (expression) => Number(expression.charAt(0)) / Number(expression.charAt(2));
-        case '%': return (expression) => Number(expression.charAt(0)) % Number(expression.charAt(2));
-    }
-}
-
-console.log(`p2(8*4): ${p2('8*4')('8*4')}`)
-
-const p3 = (str, func) => func(str)
-const p31 = p3('supercalifragilisticexpialidocious',
-    str => str.replace(/c/g, '*c').split('*') //bit stuffing => we need the c so we hide it and unhide it again; replace() returns a new string back
-
-    )
-
-console.log(`p31: ${p31}`)
-
-const p32 = str => {
-    const stringData = {
-        originalString : str,
-        modifiedString: str.replace(/a/g, 'A'),
-        numberReplaced: str.match(/a/g).length,
-        originaLength: str.length
-    }
-    return stringData;
-}
-console.log(p32('supercalifragilisticexpialidocious'))
+// //do not use var and do not use eval() EVER!!!
+//
+// const p1 = str => str.split('').sort().join('')
+// console.log(`PS1: ${p1('supercalifragilisticexpialidocious')}`)
+//
+// const p2 = expression => {
+//     const operator = expression.charAt(1);
+//     switch (operator) {
+//         case '+': return (expression) => Number(expression.charAt(0)) + Number(expression.charAt(2));
+//         case '-': return (expression) => Number(expression.charAt(0)) - Number(expression.charAt(2));
+//         case '*': return (expression) => Number(expression.charAt(0)) * Number(expression.charAt(2));
+//         case '/': return (expression) => Number(expression.charAt(0)) / Number(expression.charAt(2));
+//         case '%': return (expression) => Number(expression.charAt(0)) % Number(expression.charAt(2));
+//     }
+// }
+//
+// console.log(`p2(8*4): ${p2('8*4')('8*4')}`)
+//
+// const p3 = (str, func) => func(str)
+// const p31 = p3('supercalifragilisticexpialidocious',
+//     str => str.replace(/c/g, '*c').split('*') //bit stuffing => we need the c so we hide it and unhide it again; replace() returns a new string back
+//
+//     )
+//
+// console.log(`p31: ${p31}`)
+//
+// const p32 = str => {
+//     const stringData = {
+//         originalString : str,
+//         modifiedString: str.replace(/a/g, 'A'),
+//         numberReplaced: str.match(/a/g).length,
+//         originaLength: str.length
+//     }
+//     return stringData;
+// }
+// console.log(p32('supercalifragilisticexpialidocious'))
 
 //iterators
-const anArray = [1,2,3];
-const arrayIterator = anArray[Symbol.iterator]();
-let val = arrayIterator.next();
-console.log(`Val: ${val.value}, Flag: ${val.done}`);
-val = arrayIterator.next();
-console.log(`Val: ${val.value}, Flag: ${val.done}`);
-val = arrayIterator.next();
-console.log(`Val: ${val.value}, Flag: ${val.done}`);
-val = arrayIterator.next();
-console.log(`Val: ${val.value}, Flag: ${val.done}`);
+// const anArray = [1,2,3];
+// const arrayIterator = anArray[Symbol.iterator]();
+// let val = arrayIterator.next();
+// console.log(`Val: ${val.value}, Flag: ${val.done}`);
+// val = arrayIterator.next();
+// console.log(`Val: ${val.value}, Flag: ${val.done}`);
+// val = arrayIterator.next();
+// console.log(`Val: ${val.value}, Flag: ${val.done}`);
+// val = arrayIterator.next();
+// console.log(`Val: ${val.value}, Flag: ${val.done}`);
 
 //generators --> an iterator that you can pause
 function* listGen () {
@@ -83,6 +83,6 @@ function* fibs () {
 //Get a few fibs
 myFibs = fibs()
 let count = 5;
-while (count --> 0) { //decrement count until count goes to 0 
+while (count --> 0) { //decrement count until count goes to 0
     console.log(myFibs.next().value)
 }
