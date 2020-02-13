68981d2da9740ce1c4b2f6cb458a39080196e9d5
athinasaid <60368003+athinasaid@users.noreply.github.com>
2020-02-12 23:10:34 -0500

    Add files via upload

---
+++ feb4.js
@@ -0,0 +1,30 @@
+
+//us regular expresison on replace, take str and replace the 'c' with '*c' and split on *
+
+// let funcs = [];
+// for (var num=0; num<4; num++) {
+//     funcs.push(
+//         () => console.log(`Num: ${num}`)
+//     )
+// }
+// funcs[2];
+
+// const doEval = (str) => (someString) => (someString).split('').reverse().join('').toUpperCase()
+// let foo = doEval('8+2')('8+2')
+// console.log(foo)
+
+// const strings = ['8+2', '3-4'];
+// const evals = strings.map(value => doEval(value))
+// console.log(evals)
+
+
+//P2 use switch
+//P3 use /a/g and put it all in one const
+
+
+var str = "Is this all there is?"
+var patt1 = /is/g
+console.log(patt1)
+
+
+
